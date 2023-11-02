4a8668da834cf72253099fab8aeb6e27380a630e
Trong_Minh <trongminhmcw10e@gmail.com>
2023-11-02 23:47:54 +0700

    create a linked list in ex1

---
+++ Labwork2/ex1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct digit{
+    int value;
+    struct digit *next;
+}Digit;
+
+typedef struct unboundedInt{
+    int size;
+    Digit *head;
+}UnboundedInt;
+
+Digit *initDigit(int digit){
+    Digit *node = (Digit*) malloc(sizeof(Digit)); 
+    if(node == NULL){
+        exit(1);
+    }
+    node->value = digit;
+    node->next = NULL;
+    return node;
+}
+
+void initUnboundedInt(int SIZE,UnboundedInt *number){
+    Digit *curr;
+    int d; 
+    number->size = SIZE;
+    number->head = initDigit(1);
+    number->head->next = curr;
+    for(int i=0;i<SIZE;i++){
+        d = rand() % 10;// random from 0 to 9
+        curr->next = initDigit(d);
+        curr = curr->next;
+    }
+}
+
+void displayUnboundedInt(UnboundedInt *number){
+    for(Digit *root = number->head;root!=NULL;root = root->next){
+        printf("%d ",root->value);
+    }
+}
+
+int main(){
+    UnboundedInt *number = (UnboundedInt*) malloc(sizeof(UnboundedInt));
+    if (number==NULL){
+        exit(2);
+    }
+    initUnboundedInt(10,number);
+    
+    return 0;
+}
