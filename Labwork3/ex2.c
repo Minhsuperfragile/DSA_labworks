60cd77e59c5750d1a44ff027de5ce6a2bca3623d
Trong_Minh <trongminhmcw10e@gmail.com>
2023-11-09 00:56:52 +0700

    yay, it's worked

--- Labwork3/ex2.c
+++ Labwork3/ex2.c
@@ -54,11 +54,39 @@ void displayList(WebList *l){
 		curr = curr->next;
 	}
 }
+
+Web *moveForward(WebList *bw,WebList *fw,Web *w){
+	pushWebList(bw,w);
+	return popWebList(fw);
+}
+
+Web *moveBackward(WebList *bw,WebList *fw,Web *w){
+	pushWebList(fw,w);
+	return popWebList(bw);
+}
+
+void deleteAllStack(WebList *wl){
+	Web *curr = wl->top,*temp;
+	while(curr!=NULL){
+		temp = curr;
+		curr = curr->next;
+		free(temp);
+	}
+}
+
 int main(){
 	WebList *bwL = (WebList*) malloc(sizeof(WebList));
 	if (bwL == NULL) {
 		exit(1);
 	}
+	WebList *fwL = (WebList*) malloc(sizeof(WebList));
+	if (fwL == NULL) {
+		exit(1);
+	}
+	initWebList(fwL);
+	initWebList(bwL);
+	// initialize 2 stacks
+
 	Web *currentWeb;
 	currentWeb = initWeb("x.com","x");
 	pushWebList(bwL,currentWeb);
@@ -68,9 +96,52 @@ int main(){
 	pushWebList(bwL,currentWeb);
 	currentWeb = initWeb("facebook.com","facebook");
 	pushWebList(bwL,currentWeb);
-	displayList(bwL);
+	// add backward web to backward web list
+
+	currentWeb = initWeb("instagram.com","instagram");
+	pushWebList(fwL,currentWeb);
+	currentWeb = initWeb("stackoverflow.com","stackoverflow");
+	pushWebList(fwL,currentWeb);
+	currentWeb = initWeb("minecraft.net","minecraft");
+	pushWebList(fwL,currentWeb);
+	currentWeb = initWeb("monkeytype.com","monkeytype");
+	pushWebList(fwL,currentWeb);
+	// add forward web to forward web list
+
 	currentWeb = popWebList(bwL);
-	printf("%s (%s)\n",currentWeb->address,currentWeb->title);
+	printf("current is %s (%s)\n",currentWeb->address,currentWeb->title);
+	printf("---------------\n");
+	printf("all in backward list:\n");
 	displayList(bwL);
+	printf("---------------\n");
+	printf("all in forward list:\n");
+	displayList(fwL);
+	printf("---------------\n");
+	// pop out a web in backward list
+
+	currentWeb = moveForward(bwL,fwL,currentWeb);
+	printf("current is %s (%s) - moved forward\n",currentWeb->address,currentWeb->title);
+	printf("---------------\n");
+	printf("all in backward list:\n");
+	displayList(bwL);
+	printf("---------------\n");
+	printf("all in forward list:\n");
+	displayList(fwL);
+	printf("---------------\n");
+	// move forward and display all
+
+	currentWeb = moveBackward(bwL,fwL,currentWeb);
+	printf("current is %s (%s) - moved backward\n",currentWeb->address,currentWeb->title);
+	printf("---------------\n");
+	printf("all in backward list:\n");
+	displayList(bwL);
+	printf("---------------\n");
+	printf("all in forward list:\n");
+	displayList(fwL);
+	printf("---------------\n");
+	// move backward and display all
+
+	deleteAllStack(bwL);
+	deleteAllStack(fwL);
 	return 0;
 }
